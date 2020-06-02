c486fedaff92828d7fadd8320d7558998b36025c
MichaelSafwatHanna <michaelsafwat.hanna@gmail.com>
2020-06-02 02:23:51 +0200

    Add Pipeline Cycle Stepper Control to Form
     * Add Data Memory Grid View to trace Data Memory State.

--- Emulator/Form.Designer.cs
+++ Emulator/Form.Designer.cs
@@ -32,13 +32,18 @@
             this.userCode_textbox = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.mipsRegisters_datagridview = new System.Windows.Forms.DataGridView();
-            this.register = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.value = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.pc_textbox = new System.Windows.Forms.TextBox();
             this.PC = new System.Windows.Forms.Label();
             this.init_button = new System.Windows.Forms.Button();
             this.RunClockCycle_button = new System.Windows.Forms.Button();
+            this.dataMemory_dataGridView = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.register = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.value = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.mipsRegisters_datagridview)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataMemory_dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -55,13 +60,13 @@
             this.userCode_textbox.Location = new System.Drawing.Point(16, 46);
             this.userCode_textbox.Multiline = true;
             this.userCode_textbox.Name = "userCode_textbox";
-            this.userCode_textbox.Size = new System.Drawing.Size(211, 392);
+            this.userCode_textbox.Size = new System.Drawing.Size(386, 383);
             this.userCode_textbox.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(247, 25);
+            this.label2.Location = new System.Drawing.Point(424, 25);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(104, 17);
             this.label2.TabIndex = 2;
@@ -78,26 +83,13 @@
             this.mipsRegisters_datagridview.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.register,
             this.value});
-            this.mipsRegisters_datagridview.Location = new System.Drawing.Point(250, 46);
+            this.mipsRegisters_datagridview.Location = new System.Drawing.Point(427, 46);
             this.mipsRegisters_datagridview.Name = "mipsRegisters_datagridview";
             this.mipsRegisters_datagridview.ReadOnly = true;
+            this.mipsRegisters_datagridview.RowHeadersVisible = false;
             this.mipsRegisters_datagridview.RowTemplate.Height = 24;
-            this.mipsRegisters_datagridview.Size = new System.Drawing.Size(258, 392);
+            this.mipsRegisters_datagridview.Size = new System.Drawing.Size(201, 383);
             this.mipsRegisters_datagridview.TabIndex = 3;
-            // 
-            // register
-            // 
-            this.register.HeaderText = "Register";
-            this.register.Name = "register";
-            this.register.ReadOnly = true;
-            this.register.Width = 90;
-            // 
-            // value
-            // 
-            this.value.HeaderText = "Value";
-            this.value.Name = "value";
-            this.value.ReadOnly = true;
-            this.value.Width = 73;
             // 
             // pc_textbox
             // 
@@ -135,11 +127,72 @@
             this.RunClockCycle_button.UseVisualStyleBackColor = true;
             this.RunClockCycle_button.Click += new System.EventHandler(this.RunClockCycle_button_Click);
             // 
+            // dataMemory_dataGridView
+            // 
+            this.dataMemory_dataGridView.AllowUserToAddRows = false;
+            this.dataMemory_dataGridView.AllowUserToDeleteRows = false;
+            this.dataMemory_dataGridView.AllowUserToResizeColumns = false;
+            this.dataMemory_dataGridView.AllowUserToResizeRows = false;
+            this.dataMemory_dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataMemory_dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.address,
+            this.dataValue});
+            this.dataMemory_dataGridView.Location = new System.Drawing.Point(657, 46);
+            this.dataMemory_dataGridView.Name = "dataMemory_dataGridView";
+            this.dataMemory_dataGridView.ReadOnly = true;
+            this.dataMemory_dataGridView.RowHeadersVisible = false;
+            this.dataMemory_dataGridView.RowTemplate.Height = 24;
+            this.dataMemory_dataGridView.Size = new System.Drawing.Size(180, 383);
+            this.dataMemory_dataGridView.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(654, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Data Memory";
+            // 
+            // address
+            // 
+            this.address.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.address.HeaderText = "Address";
+            this.address.Name = "address";
+            this.address.ReadOnly = true;
+            this.address.Width = 89;
+            // 
+            // dataValue
+            // 
+            this.dataValue.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.dataValue.HeaderText = "Value";
+            this.dataValue.Name = "dataValue";
+            this.dataValue.ReadOnly = true;
+            this.dataValue.Width = 73;
+            // 
+            // register
+            // 
+            this.register.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.register.HeaderText = "Register";
+            this.register.Name = "register";
+            this.register.ReadOnly = true;
+            this.register.Width = 90;
+            // 
+            // value
+            // 
+            this.value.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.value.HeaderText = "Value";
+            this.value.Name = "value";
+            this.value.ReadOnly = true;
+            this.value.Width = 73;
+            // 
             // Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 519);
+            this.ClientSize = new System.Drawing.Size(1081, 519);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataMemory_dataGridView);
             this.Controls.Add(this.RunClockCycle_button);
             this.Controls.Add(this.init_button);
             this.Controls.Add(this.PC);
@@ -151,6 +204,7 @@
             this.Name = "Form";
             this.Text = "MIPS Emulator";
             ((System.ComponentModel.ISupportInitialize)(this.mipsRegisters_datagridview)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataMemory_dataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -162,12 +216,16 @@
         private System.Windows.Forms.TextBox userCode_textbox;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.DataGridView mipsRegisters_datagridview;
-        private System.Windows.Forms.DataGridViewTextBoxColumn register;
-        private System.Windows.Forms.DataGridViewTextBoxColumn value;
         private System.Windows.Forms.TextBox pc_textbox;
         private System.Windows.Forms.Label PC;
         private System.Windows.Forms.Button init_button;
         private System.Windows.Forms.Button RunClockCycle_button;
+        private System.Windows.Forms.DataGridView dataMemory_dataGridView;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn register;
+        private System.Windows.Forms.DataGridViewTextBoxColumn value;
+        private System.Windows.Forms.DataGridViewTextBoxColumn address;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataValue;
     }
 }
 
--- Emulator/Form.cs
+++ Emulator/Form.cs
@@ -1,5 +1,6 @@
+﻿using MIPS;
 using System;
-using MIPS;
+using System.Windows.Forms;
 
 namespace Emulator
 {
@@ -14,21 +15,34 @@ namespace Emulator
 
         private void Init_button_Click(object sender, EventArgs e)
         {
-            _cpu = new Mips();
-            for (var i = 0; i < _cpu.RegisterFile.Registers.Length; i++)
-            {
-                mipsRegisters_datagridview.Rows.Add(i.ToString(), _cpu.RegisterFile.Registers[i].ToString());
-            }
-            pc_textbox.Text = _cpu.Pc.ToString();
-            userCode_textbox.Text = $@"{_cpu.Pc}: ";
+            pc_textbox.Text = "1000";
+            _cpu = new Mips(uint.Parse(pc_textbox.Text));
+            RefreshRegistersGridView();
+            userCode_textbox.Text = $@"{pc_textbox.Text}: ";
         }
 
         private void RunClockCycle_button_Click(object sender, EventArgs e)
         {
+            if (_cpu == null)
+            {
+                MessageBox.Show("Initialize Emulator First!");
+                return;
+            }
+
+            if (userCode_textbox.Text == "")
+            {
+                MessageBox.Show("Can't Run Emulator while user code is empty!");
+                return;
+            }
+
             if (_cpu.Clock == 0)
             {
                 PopulateInstructionMemory(userCode_textbox.Text);
             }
+
+            _cpu.RunClockCycle();
+            RefreshRegistersGridView();
+            RefreshDataMemoryGridView();
         }
 
         private void PopulateInstructionMemory(string instructions)
@@ -48,6 +62,24 @@ namespace Emulator
             {
                 lastAddress += 4;
                 _cpu.InstructionMemory.Add(lastAddress, new string('0', 32));
+            }
+        }
+
+        private void RefreshRegistersGridView()
+        {
+            mipsRegisters_datagridview.Rows.Clear();
+            for (var i = 0; i < _cpu.RegisterFile.Registers.Length; i++)
+            {
+                mipsRegisters_datagridview.Rows.Add($"${i}", _cpu.RegisterFile.Registers[i].ToString());
+            }
+        }
+
+        private void RefreshDataMemoryGridView()
+        {
+            dataMemory_dataGridView.Rows.Clear();
+            foreach (var pair in _cpu.DataMemory)
+            {
+                dataMemory_dataGridView.Rows.Add(pair.Key, pair.Value);
             }
         }
     }
